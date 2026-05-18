a6774fa3d92ea9d06d016036ae3d70d38fdb9639
abishaiii <128905673+abishaiii@users.noreply.github.com>
2026-05-19 03:22:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// This forces Vite to treat your main root folder as the code path
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  build: {
+    outDir: 'dist',
+  }
 })
